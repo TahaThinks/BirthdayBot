45e0c110c558fc254ec3463d77499c2e3f8172ac
TahaThinks <tahaa4587@gmail.com>
2024-04-16 19:22:04 +0400

    Close Connections

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from passwords import *
 connection = smtplib.SMTP("smtp.gmail.com", port=587)
 connection.starttls()
 connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs=recipient_email, msg="hello")
+connection.sendmail(from_addr=my_email, to_addrs=recipient_email, msg="Subject:Hello")
+connection.close()
