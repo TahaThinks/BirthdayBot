33814a711833d382b52e0cb9c4de22b5672f4b93
TahaThinks <tahaa4587@gmail.com>
2024-04-16 19:56:52 +0400

    Send an email of Random Quotes

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-# import smtplib
-# from passwords import *
-#
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=my_password)
-#     connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
-#                         msg="Subject:Hello\n\nThis is the body of the email.\n Taha Ahmed Hussein")
-
+import smtplib
+from passwords import *
 import datetime as dt
-monday = 0
+from random import choice
+
+# Get Day of the Week
+monday = 1
 now = dt.datetime.now()
 day_of_week = now.weekday()
 
+# Get Quote
+with open("quotes.txt") as file:
+    lines = file.readlines()
+
+
+
 if day_of_week == monday:
-    print("Hello, Taha")
-else:
-    print("Its not Monday")
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
+                            msg=f"Subject:Quote of the Day\n\n{choice(lines)}\n\n\n Taha Ahmed Hussein")
