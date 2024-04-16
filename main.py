4586d123aa415966706716c5a0dee419476afebf
TahaThinks <tahaa4587@gmail.com>
2024-04-16 19:59:25 +0400

    Refactor COde

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from passwords import *
 import datetime as dt
 from random import choice
 
+def send_mail(message):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
+                            msg=f"Subject:Quote of the Day\n\n{message}\n\n\n Taha Ahmed Hussein")
+
 # Get Day of the Week
-monday = 1
+tuesday = 1
 now = dt.datetime.now()
 day_of_week = now.weekday()
 
@@ -12,11 +19,7 @@ day_of_week = now.weekday()
 with open("quotes.txt") as file:
     lines = file.readlines()
 
-
-
-if day_of_week == monday:
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=my_password)
-        connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
-                            msg=f"Subject:Quote of the Day\n\n{choice(lines)}\n\n\n Taha Ahmed Hussein")
+if day_of_week == tuesday:
+    send_mail(choice(lines))
+else:
+    pass
