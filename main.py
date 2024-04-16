6bb84f62a2c5d7987beff3fdc967be5a1b1daa92
TahaThinks <tahaa4587@gmail.com>
2024-04-16 19:08:09 +0400

    Sending Email

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Starting File")
+import smtplib
+from passwords import *
+
+connection = smtplib.SMTP("smtp.gmail.com", port=587)
+connection.starttls()
+connection.login(user=my_email, password=my_password)
+connection.sendmail(from_addr=my_email, to_addrs=recipient_email, msg="hello")
