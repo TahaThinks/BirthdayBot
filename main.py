10e210799f1d741307a2a01121d681f2fc298a21
TahaThinks <tahaa4587@gmail.com>
2024-04-16 19:59:41 +0400

    Refactor COde

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from passwords import *
 import datetime as dt
 from random import choice
 
+
 def send_mail(message):
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(user=my_email, password=my_password)
         connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
                             msg=f"Subject:Quote of the Day\n\n{message}\n\n\n Taha Ahmed Hussein")
+
 
 # Get Day of the Week
 tuesday = 1
