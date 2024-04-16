792f63d54f030fd27c1e6d04257097c3dc5487f2
TahaThinks <tahaa4587@gmail.com>
2024-04-16 19:47:26 +0400

    Get Date of Birth

--- main.py
+++ main.py
@@ -10,4 +10,8 @@
 import datetime as dt
 
 now = dt.datetime.now()
-print(now)
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=1996 ,month=4 ,day=17)
+print(date_of_birth)
